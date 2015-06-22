26418a23db980638d67109f6f3ac155da55cccee
David Horn <horndavidg@gmail.com>
2015-06-22 16:25:22 -0700

    login and signup page first version with basic auth

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(session({
   // (cookie specific)
 }));
 
-//******************* PLACE ROUTES *************************//
+//******************* USER ROUTES *************************//
 
 
 // ROOT (PUBLIC) //
@@ -41,16 +41,76 @@ app.get('/', function(req,res){
   res.render("users");
 });
 
+// USER NEEDS TO SIGN UP! //
+
+app.get('/signup', routeMiddleware.preventLoginSignup ,function(req,res){
+    var clear = "";
+    res.render("users/signup", {err:clear});
+    // added clean err var to render page
+});
+
+// USER SUBMITTS SIGN UP FORM! // 
+
+app.post("/signup", function (req,res) {
+  var newUser = req.body.user;
+  db.User.create(newUser, function (err, user) {
+    if (user) {
+      req.login(user);
+      res.redirect("/places");
+    } else if (err){
+      
+        // Conditional logic for err handling here!
+       // ERROR HANDLING - SEE VIEWS AND MODELS FILES
+      res.render("users/signup", {err:err});
+
+   }
+  });
+});
+
+// DIRECTED TO THE LOGIN PAGE //
+
+app.get("/login", routeMiddleware.preventLoginSignup, function (req, res) {
+  var clear = "";
+  res.render("users/login", {err:clear});
+});
+
+// USER SUBMITTS LOGIN FORM! //
+
+app.post("/login", function (req, res) {
+  db.User.authenticate(req.body.user,
+  function (err, user) {
+    if (!err && user !== null) {
+      req.login(user);
+      res.redirect("/places");
+    } else {
+      
+      // ERROR HANDLING - SEE VIEWS AND MODELS FILES
+      res.render("users/login", {err:err});
+    }
+  });
+});
+
+
+// LOGS OUT USER! //
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 
 
 
 
 
+//******************* PLACE ROUTES *************************//
 
 
+// INDEX ROUTE //
 
-
+app.get('/places', function(req,res){
+res.render("places/index", {currentuser:""});
+});
 
 
 
---
+++ models/user.js
@@ -0,0 +1,93 @@
+var bcrypt = require("bcrypt");
+var SALT_WORK_FACTOR = 10;
+var mongoose = require("mongoose");
+
+var userSchema = new mongoose.Schema({
+      username: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true
+
+    },
+
+    email: {
+      type: String,
+      lowercase: true,
+      required: true,
+      unique: true
+
+    },
+    
+    password: {
+      type: String,
+      required: true
+    },
+  });
+
+
+// hook that runs before the user saves
+userSchema.pre('save', function(next) {
+  var user = this;
+  // refers to the instance of the user
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+// if password hasn't been modified move on and save user
+
+  return bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) {
+      return next(err);
+    }
+    return bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) {
+        return next(err);
+      }
+      
+// final step - user.password refers to whatever was passed through via a form.
+      user.password = hash;
+      return next();
+    });
+  });
+});
+
+
+// statics === CLASS METHODS
+
+userSchema.statics.authenticate = function (formData, callback) {
+  this.findOne({ // this = db.user
+      // find an existing user with their e-mail.
+      email: formData.email
+    },
+    // helper function
+    function (err, user) {
+      if (user === null){
+        callback("Invalid username or password",null);
+        // dosen't give away too much information
+      }
+      else {
+        user.checkPassword(formData.password, callback);
+      }
+
+    });
+};
+
+
+// INSTANCE METHOD //
+
+userSchema.methods.checkPassword = function(password, callback) {
+  var user = this;
+  // this refers to the instance of the user
+  bcrypt.compare(password, this.password, function (err, isMatch) {
+    if (isMatch) {
+      callback(null, user);
+    } else {
+      callback(err, null);
+    }
+  });
+};
+
+var User = mongoose.model("User", userSchema);
+
+module.exports = User;
