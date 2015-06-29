2d53aba1a069859d99aa69599a43947e2fc74d08
David Horn <horndavidg@gmail.com>
2015-06-29 13:21:32 -0700

    setting up mongo production db and env's

--- public/js/show.js
+++ public/js/show.js
@@ -13,7 +13,7 @@ var location = $('#location').val();
 var key = $('#key').val();
 
 $.ajax({
-  url: 'https://terminal2.expedia.com/x/activities/search?location=' + location + '&apikey=' + key,
+  url: 'http://terminal2.expedia.com/x/activities/search?location=' + location + '&apikey=' + key,
   method: "GET",
   success: function(data) {
   var info = JSON.stringify(data);
